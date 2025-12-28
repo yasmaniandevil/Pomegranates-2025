05c392f7e3e09409a7f0aef395b050df01c6bb5d
Tae (Sydra) HAssoun <thassoun000@gmail.com>
2025-12-27 20:25:00 -0500

    Hospital Prologue Cutscene

---
+++ Pomegranates2025/Assets/Scripts/Cutscene/CutsceneManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class CutsceneManager : MonoBehaviour
+{
+    private PlayableDirector playableDirector;
+
+    public TimelineAsset[] timelineAssets;
+    private int timelineAssetInd;
+    void Awake()
+    {
+        playableDirector = GetComponent<PlayableDirector>();
+        timelineAssetInd = 0;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        playableDirector.Play(timelineAssets[timelineAssetInd]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // None Here
+    }
+
+    public void PlayNext()
+    {
+        timelineAssetInd++;
+        playableDirector.Play(timelineAssets[timelineAssetInd]);
+    }
+}
